581016be18864c3cc7a1e581b77cddd98d7ac460
Alex McKendrick <48360400+Hexagonsun70@users.noreply.github.com>
2021-07-15 11:02:01 +0100

    debug push

--- js/axios.js
+++ js/axios.js
@@ -2,11 +2,23 @@ const URL = 'https://picsum.photos/300/200';
 
 let IMG = document.getElementById('img');
 
-document.getElementById('random-btn').onclick = axios.get(URL)
-.then(response=> {
-  let picsum = response.headers['picsum-id'];
-  console.log(picsum);
-  let path  = `https://picsum.photos/id/${picsum}/300/200`;
-  IMG.src = path;
-  return path;
+const axiosImgGet = () => axios.get(URL)
+.then((response) => {
+  picID = response.headers['picsum-id'];
+  picURL = `https://picsum.photos/id/${picID}/300/200`;
+  IMG.src = picURL;
 });
+
+console.log(axiosImgGet());
+
+// axios.get(URL)
+//   .then(response=> {
+//     console.log("Initialised get")
+//     let picsum = response.headers['picsum-id'];
+//     console.log(picsum);
+//     let path  = `https://picsum.photos/id/${picsum}/300/200`;
+//     IMG.src = path;
+//     return path;
+//   });
+
+// document.getElementById('random-btn').onclick
