66e4a90bf42f1528b204920e4b07919e1b53e03d
Alex McKendrick <48360400+Hexagonsun70@users.noreply.github.com>
2021-07-23 11:30:10 +0100

    minor changes

--- js/axios.js
+++ js/axios.js
@@ -66,16 +66,7 @@ document.getElementById('random-btn').onclick = function(){
   axiosImgGet()
 };
 
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function() {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("myBtn").click();
-  }
-});
+
 
 
 document.getElementById('save-btn').onclick = function(){
